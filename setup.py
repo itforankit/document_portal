89040c3de7952a0238fd8b300a5e0e319c712c8b
Ankit Singhal <itforankit@gmail.com>
2025-07-30 07:33:07 +0530

    mac machine

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="document_portal",
+<<<<<<< HEAD
     author="Ankit Singhal",
+=======
+    author="Sunny Savita",
+>>>>>>> effe946 (mac machine)
     version="0.1",
     packages=find_packages(),
 )
